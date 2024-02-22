3e732b26e3642ebc6838dc6d1bdfaf1b77216c4a
Abdul-Rehman636 <rehmankhan636@gmail.com>
2024-02-22 22:46:47 +0500

    Convertor App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const [text, setText] = useState(para);
+
+  function para() {
+    alert("Your final answer is " + count);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='div-1'>
+      <div id='div-2'>
+        <button id='button-1' onClick={() => {
+          setCount(count+1)
+        }}>Increase +</button>
+        <div id='div-3'>
+          <h1>{count}</h1>
+        </div>
+        <button id='button-2' onClick={() => {
+          setCount(count-1)
+        }}>Decrease -</button>
+        <button id='button-3' onClick={para}>Submit</button>
+      </div>
     </div>
   );
 }
